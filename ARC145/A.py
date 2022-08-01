7a3175059fb7dd87dd9e31c3a87c2b8e0f12db33
toona-laptop <amanou.toona@gmail.com>
2022-08-01 09:40:53 +0900

    Late sub: ARC145

--- ARC145/A.py
+++ ARC145/A.py
@@ -4,33 +4,17 @@ import sys
 def main():
     _ = int(input())
     S = sys.stdin.readline().strip()
-    S = list(S)
 
-    S1 = S.copy()
-    S2 = list(reversed(S))
-
-    if S1 == S2:
-        print("Yes")
+    if S == "AB" or S == "BA":
+        print("No")
         return
 
-    for i, (s1, s2) in enumerate(zip(S1, S2)):
-        if s1 == "B":
-            if s2 == "B":
-                print("Yes")
-                return
-
-            if i == (len(S) - 1) // 2:
-                print("No")
-                return
-
-            print("Yes")
-            return
-
-        if s2 == "B":
-            print("No")
-            return
+    if S[0] == "A" and S[-1] == "B":
+        print("No")
+        return
 
     print("Yes")
+    return
 
 
 if __name__ == "__main__":
